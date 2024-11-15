fef39ce7172ab0a2a47576970011112ce61b35ab
William Ramberg <117517479+WillRamberg@users.noreply.github.com>
2024-11-15 02:44:51 +0100

    Crazy weird log in error

---
+++ frontend/src/Diagnosis.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Diagnosis: React.FC = () => {
+  return (
+    <div>
+      <h1>Create Diagnosis</h1>
+      <p>You are on the Create Diagnosis Page</p>
+    </div>
+  );
+};
+
+export default Diagnosis;
