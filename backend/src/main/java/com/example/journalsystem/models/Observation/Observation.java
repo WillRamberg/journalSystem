c0dff2b1f7484fb347b98af916a74d88df385a19
sneeky-panda <127853393+sneeky-panda@users.noreply.github.com>
2024-11-15 16:01:40 +0100

    madel uppdatering

--- backend/src/main/java/com/example/journalsystem/models/Observation/Observation.java
+++ backend/src/main/java/com/example/journalsystem/models/Observation/Observation.java
@@ -1,16 +1,17 @@
 package com.example.journalsystem.models.Observation;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class Observation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String description;
-    private LocalDate observationDate;
+    private Long observation_id;
+    private String observation_description;
+    private LocalDate observation_date;
 }
